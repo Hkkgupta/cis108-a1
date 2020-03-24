328f60ce91f7a3a69bba8ee97915b93d80a386cf
Will Smith <wismith@davidson.edu>
2020-03-24 07:02:27 -0500

    max problem

--- src/max.js
+++ src/max.js
@@ -6,8 +6,13 @@
  * @returns {number} The largest number
  */
 function max(array) {
-  // This is your job. :)
-  // Don't use the built-in Math.max
+  let runningLargest = array[0];
+  for (let item of array) {
+    if (item > runningLargest) {
+      runningLargest = item;
+    }
+  }
+  return runningLargest;
 }
 
 if (require.main === module) {
@@ -15,6 +20,36 @@ if (require.main === module) {
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
+  let tests = [
+    {
+      array: [1,2,3,4,5],
+      correct: 5
+    },
+    {
+      array: [5,4,3,2,1],
+      correct: 5
+    },
+    {
+      array: [-2,-1,0,1,2],
+      correct: 2,
+    },
+    {
+      array: [5,5,5,5,5],
+      correct: 5,
+    },
+    {
+      array: [-5,-4,-3,-2,-1],
+      correct: -1,
+    },
+    {
+      array: [0,0,0,0,0],
+      correct: 0
+    }
+  ];
+
+  for (let test of tests) {
+    console.log(max(test.array) === test.correct);
+  }
 }
 
 module.exports = max;
