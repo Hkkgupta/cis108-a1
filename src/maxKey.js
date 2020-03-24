7b7a999df3a1823514107da959339d74c9cb7ecf
Will Smith <wismith@davidson.edu>
2020-03-24 07:36:36 -0500

    maxKey problem

--- src/maxKey.js
+++ src/maxKey.js
@@ -8,15 +8,46 @@
  * @param {object} array - A non-empty array of numbers
  * @returns {number} The key with the largest value
  */
+
 function maxKey(obj) {
-  // This is your job. :)
+  let runningLargest;
+  for (let entry in obj) {
+    if (obj.hasOwnProperty(entry)) {
+      if (runningLargest) {
+        if (obj[entry] > obj[runningLargest]) {
+          runningLargest = entry;
+        }
+      } else {
+        runningLargest = entry;
+      }
+    }
+  }
+  return runningLargest;
 }
+
 
 if (require.main === module) {
   console.log('Running sanity checks for maxKey:');
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
+  let tests = [
+    {
+      obj: {'a': 1, 'b': 2,'c': 3},
+      expected: 'c'
+    },
+    {
+      obj: {'hello': -10,'world': 10,'this': 5,'is': 15,'a': 2,'test': 0},
+      expected: 'is'
+    },
+    {
+      obj: {'a': -1,'b': -2,'c': 0,'d': 1,'e': 2},
+      expected: 'e'
+    }
+  ];
+  for (let test of tests) {
+    console.log(maxKey(test.obj) === test.expected);
+  }
 }
 
 module.exports = maxKey;
