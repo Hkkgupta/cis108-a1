24ca4955510a51dfa34066b045eb7228b4ec8fa6
Will Smith <wismith@davidson.edu>
2020-03-24 07:50:38 -0500

    smallestPrimeFactor

--- src/smallestPrimeFactor.js
+++ src/smallestPrimeFactor.js
@@ -12,7 +12,21 @@
  * @returns {number} The smallest prime factor of num
  */
 function smallestPrimeFactor(num) {
-  // This is your job. :)
+  /* const primeFactors = [];
+  let workingNum = num;
+  for (let n = 2; n <= num/2; n++) {
+    while (workingNum % n === 0) {
+      primeFactors.push(n);
+      workingNum /= n;
+    }
+
+  } */
+
+  for (let n = 2; n <= num; n++) {
+    if (num % n === 0) {
+      return n;
+    }
+  }
 }
 
 if (require.main === module) {
@@ -20,6 +34,39 @@ if (require.main === module) {
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
+  let tests = [
+    {
+      num: 2,
+      expected: 2
+    },
+    {
+      num: 4,
+      expected: 2
+    },
+    {
+      num: 5,
+      expected: 5,
+    },
+    {
+      num: 9,
+      expected: 3,
+    },
+    {
+      num: 17,
+      expected: 17
+    },
+    {
+      num: 25,
+      expected: 5
+    },
+    {
+      num: 57,
+      expected: 3
+    }
+  ];
+  for (let test of tests) {
+    console.log(smallestPrimeFactor(test.num) === test.expected);
+  }
 }
 
 module.exports = smallestPrimeFactor;
